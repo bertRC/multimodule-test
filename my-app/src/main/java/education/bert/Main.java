f63c91035afa4c1bbd17b1dac355efa6c424b6f7
bertRC <sithalbert@ya.ru>
2020-01-08 15:41:04 +0300

    my-app module with tests added

---
+++ my-app/src/main/java/education/bert/Main.java
@@ -0,0 +1,8 @@
+package education.bert;
+
+public class Main {
+    public static void main(String[] args) {
+        Summator summator = new Summator();
+        System.out.println(summator.sum(1, 2));
+    }
+}
---
+++ my-app/src/test/java/education/bert/MainTest.java
@@ -0,0 +1,13 @@
+package education.bert;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MainTest {
+    @Test
+    public void mainTest() {
+        String text = "Hello World!";
+        assertNotNull(text);
+    }
+}
